3df75eb005c5e30734fa1a4c365c43fbae936af6
Glitch (hello-express) <none>
2020-06-10 15:03:24 +0000

    🛣💧 Checkpoint ./server.js:614688/91 ./config.json:614688/74 ./package.json:614688/88

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+console.log('hi');
+
+const { Client } = require("discord.js");
+const client = new Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
@@ -31,4 +33,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN)
