64f9bb59349a63607d04192157f69820fea6635a
Glitch (hello-express) <none>
2020-06-25 12:49:24 +0000

    🚇🕴 Checkpoint ./noidea.js:17431536/439 ./package.json:17431536/24

--- noidea.js
+++ noidea.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
-
 client.on('ready', () => {
-  client.channels.cache.get("562896458496344089").send(";-;")
+  console.log("Up and ready to rock")
+})
+
+client.on('message', async message => {
+  if (message.content.includes("Go ahead")){
+    const messageToSend = message.content.replace("Go ahead ", "")
+    client.channels.cache.get("562896458496344089").send(messageToSend)
+  }
 })
 
 client.login(process.env.TOKEN)
