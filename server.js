20b9062b047b49d2627b1bf7346b2226a3fbe5bb
Glitch (hello-express) <none>
2020-06-26 08:07:43 +0000

    🔮🍰 Checkpoint ./package.json:17431536/14 ./server.js:17431536/11

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const ColorScheme = require("color-scheme");
+/*const ColorScheme = require("color-scheme");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
@@ -361,3 +361,4 @@ client.on("message", async message => {
 });
 
 client.login(process.env.TOKEN);
+*/ tttt
